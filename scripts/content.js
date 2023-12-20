0c5d3d3c7cf802c27096707adcc6c8467fc6f092
unknown <subhajit1742@gmail.com>
2023-12-20 16:57:29 +0530

    Initial commit

---
+++ scripts/content.js
@@ -0,0 +1,11 @@
+document.addEventListener("click",()=>{
+    chats = document.getElementsByClassName("_2Ts6i _2xAQV")[0];
+    uE = document.createElement("div");
+    uE.classList.add("upperElement");
+    
+    uE.style.cssText = 'position: absolute;width: 100vw;height: 100vh;z-index: 1;backdrop-filter: blur(12px);';
+    
+    chats?.insertAdjacentElement("beforeend",uE);
+    
+    console.log("chalja bhai plz");
+})
